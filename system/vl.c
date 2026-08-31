b407f5c6add9f86692aa809c97bad0709fd78ee6
Yonggang Luo <luoyonggang@gmail.com>
2026-08-31 14:26:51 +0800

    system/vl: add new option -target
    - Parse -target with QEMU_OPTION_nouserconfig, right after
      MODULE_INIT_TARGET_INFO, because later init depends on TargetInfo.
    - target_info_qom_set_target() takes Error **errp. A name comes from
      -target or from argv0 (qemu-system-aarch64).
    - Bare qemu-system with no suffix and no -target leaves
      target_arch() as SYS_EMU_TARGET_NONE. qemu-system-* still selects
      from the argv[0] suffix.
    - -M help lists the union when target_arch() is SYS_EMU_TARGET_NONE.
      Starting a real board without a target errors; -M none is allowed.
    - qemu_arch_available() is true when target_arch() is
      SYS_EMU_TARGET_NONE.
    - Fuzz passes TARGET_NAME instead of relying on a unique TargetInfo.
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>

--- system/vl.c
+++ system/vl.c
@@ -281,6 +281,15 @@ static QemuOptsList qemu_accel_opts = {
     },
 };
 
+static QemuOptsList qemu_target_opts = {
+    .name = "target",
+    .implied_opt_name = "target",
+    .head = QTAILQ_HEAD_INITIALIZER(qemu_target_opts.head),
+    .desc = {
+        { /* end of list */ },
+    },
+};
+
 static QemuOptsList qemu_boot_opts = {
     .name = "boot-opts",
     .implied_opt_name = "order",
@@ -2896,6 +2905,7 @@ void qemu_init(int argc, char **argv)
     const char *optarg;
     MachineClass *machine_class;
     bool userconfig = true;
+    const char *target_name_option = NULL;
     FILE *vmstate_dump_file = NULL;
 
     qemu_add_opts(&qemu_drive_opts);
@@ -2929,6 +2939,7 @@ void qemu_init(int argc, char **argv)
     qemu_add_opts(&qemu_semihosting_config_opts);
     qemu_add_opts(&qemu_fw_cfg_opts);
     qemu_add_opts(&qemu_action_opts);
+    qemu_add_opts(&qemu_target_opts);
     qemu_add_run_with_opts();
     module_call_init(MODULE_INIT_OPTS);
 
@@ -2938,13 +2949,6 @@ void qemu_init(int argc, char **argv)
     os_setup_limits();
 
     module_call_init(MODULE_INIT_TARGET_INFO);
-    target_info_qom_set_target();
-
-    module_init_info(qemu_modinfo);
-    module_allow_arch(target_name());
-
-    qemu_init_subsystems();
-
     /* first pass of option parsing */
     optind = 1;
     while (optind < argc) {
@@ -2959,9 +2963,19 @@ void qemu_init(int argc, char **argv)
             case QEMU_OPTION_nouserconfig:
                 userconfig = false;
                 break;
+            case QEMU_OPTION_target:
+                target_name_option = optarg;
+                break;
             }
         }
     }
+
+    target_info_qom_set_target(target_name_option, &error_fatal);
+
+    module_init_info(qemu_modinfo);
+    module_allow_arch(target_name());
+
+    qemu_init_subsystems();
 
     machine_opts_dict = qdict_new();
     if (userconfig) {
@@ -2986,6 +3000,9 @@ void qemu_init(int argc, char **argv)
                 exit(1);
             }
             switch(popt->index) {
+            case QEMU_OPTION_target:
+                /* handled previously, ignore it here */
+                break;
             case QEMU_OPTION_cpu:
                 /* hw initialization will check this */
                 cpu_option = optarg;
