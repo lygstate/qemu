d9758e944d86cd26fd1dba77ae359b5c5b490b7d
Yonggang Luo <luoyonggang@gmail.com>
2026-08-31 14:20:09 +0800

    Revert "system/vl: fallback to detect target from argv[0]"
    This reverts commit 22441bc3536708fbb6e7ed25a2a933541047b4d0.

--- system/vl.c
+++ system/vl.c
@@ -2926,7 +2926,7 @@ void qemu_init(int argc, char **argv)
         }
     }
     if (!target_info()) {
-        target_info_qom_set_target_from_argv0(argv[0]);
+        target_info_qom_set_target();
     }
 
     module_init_info(qemu_modinfo);
