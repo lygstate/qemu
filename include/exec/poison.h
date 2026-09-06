a710ca4718584c44e40920c96ba86934165247f8
Yonggang Luo <luoyonggang@gmail.com>
2026-09-07 06:17:46 +0800

    meson: build one host-accel lib with TARGET_* poisoned
    - Split TARGET_* pragma GCC poison into exec/poison-target-arch.h.
    - Include that file from osdep.h under COMPILING_PER_TARGET_BASE
      (only valid under COMPILING_PER_TARGET).
    - Build one host-accel-$base lib per TARGET_BASE_ARCH from the
      CONFIG_* union, extract objects for each matching softmmu, and
      error if more than one host-accel family is produced.
    - Declare named accel_*_ss for arm, i386, loongarch, ppc, riscv,
      and s390x so subdirs can add host-accel guest glue.
    - Define COMPILING_PER_TARGET_BASE immediately after
      COMPILING_PER_TARGET in host-accel c_args.
    - Under COMPILING_PER_TARGET_BASE, omit MO_TE enum values in
      memop.h and provide runtime mo_te() plus MO_TE* macros.
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>

--- include/exec/poison.h
+++ include/exec/poison.h
@@ -5,43 +5,10 @@
 #define HW_POISON_H
 
 #include "config-poison.h"
-
-#pragma GCC poison TARGET_I386
-#pragma GCC poison TARGET_X86_64
-#pragma GCC poison TARGET_AARCH64
-#pragma GCC poison TARGET_ALPHA
-#pragma GCC poison TARGET_ARM
-#pragma GCC poison TARGET_HEXAGON
-#pragma GCC poison TARGET_HPPA
-#pragma GCC poison TARGET_LOONGARCH64
-#pragma GCC poison TARGET_M68K
-#pragma GCC poison TARGET_MICROBLAZE
-#pragma GCC poison TARGET_MIPS
-#pragma GCC poison TARGET_ABI_MIPSN32
-#pragma GCC poison TARGET_ABI_MIPSO32
-#pragma GCC poison TARGET_MIPS64
-#pragma GCC poison TARGET_ABI_MIPSN64
-#pragma GCC poison TARGET_OR1K
-#pragma GCC poison TARGET_PPC
-#pragma GCC poison TARGET_PPC64
-#pragma GCC poison TARGET_ABI32
-#pragma GCC poison TARGET_RX
-#pragma GCC poison TARGET_S390X
-#pragma GCC poison TARGET_SH4
-#pragma GCC poison TARGET_SPARC
-#pragma GCC poison TARGET_SPARC64
-#pragma GCC poison TARGET_TRICORE
-#pragma GCC poison TARGET_XTENSA
+#include "exec/poison-target-arch.h"
 
 #pragma GCC poison TARGET_HAS_BFLT
-#pragma GCC poison TARGET_NAME
-#pragma GCC poison TARGET_BIG_ENDIAN
 #pragma GCC poison TCG_GUEST_DEFAULT_MO
-
-#pragma GCC poison TARGET_LONG_BITS
-#pragma GCC poison TARGET_FMT_lx
-#pragma GCC poison TARGET_FMT_ld
-#pragma GCC poison TARGET_FMT_lu
 
 #pragma GCC poison TARGET_PHYS_ADDR_SPACE_BITS
 
