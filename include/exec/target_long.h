a6a069e700718f16ff3bb8e60b4c63c6aa6c88e1
Yonggang Luo <luoyonggang@gmail.com>
2026-09-12 00:09:23 +0800

    exec: allow target_ulong and packed arrays outside per-target compiles
    - Add a common-system else arm that typedefs target_ulong to uint64_t
      so helper tl and GP values compile without TARGET_LONG_BITS.
    - Keep the per-target 32/64 typedefs, TARGET_FMT_*, and MO_TL.
    - Add TARGET_*_ARRAY unions and accessors that pick stride from
      target_long_bits(); do not add scalar target_*_t unions.
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>

--- include/exec/target_long.h
+++ include/exec/target_long.h
@@ -10,6 +10,7 @@
 #ifndef _TARGET_LONG_H_
 #define _TARGET_LONG_H_
 
+#ifdef COMPILING_PER_TARGET
 /*
  * Usually this should only be included via cpu-defs.h however for
  * certain cases where we want to build only two versions of a binary
@@ -40,5 +41,87 @@ typedef uint64_t target_ulong;
 #else
 #error TARGET_LONG_SIZE undefined
 #endif
+#else
+typedef uint64_t target_ulong;
+#endif
+
+#include "qemu/target-info.h"
+
+/*
+ * 1-element view of a packed target_long array. Pass as
+ * target_long_array_t *; index i with the packed s32/s64 view and
+ * target_long_bits(). Do not use sizeof(*p) as length or stride.
+ */
+typedef union {
+    int32_t s32[1];
+    int64_t s64[1];
+} target_long_array_t;
+
+typedef union {
+    uint32_t u32[1];
+    uint64_t u64[1];
+} target_ulong_array_t;
+
+#define TARGET_LONG_ARRAY(n) \
+    union { \
+        int32_t s32[n]; \
+        int64_t s64[n]; \
+        target_long_array_t rec; \
+    }
+#define TARGET_ULONG_ARRAY(n) \
+    union { \
+        uint32_t u32[n]; \
+        uint64_t u64[n]; \
+        target_ulong_array_t rec; \
+    }
+
+static inline uint64_t target_ulong_array_val(const target_ulong_array_t *p,
+                                              unsigned i)
+{
+    if (target_long_bits() == 64) {
+        return p->u64[i];
+    }
+    return p->u32[i];
+}
+
+static inline uint64_t target_ulong_array_set(target_ulong_array_t *p,
+                                              unsigned i, uint64_t v)
+{
+    if (target_long_bits() == 64) {
+        p->u64[i] = v;
+        return v;
+    }
+    p->u32[i] = (uint32_t)v;
+    return p->u32[i];
+}
+
+static inline int64_t target_long_array_val(const target_long_array_t *p,
+                                            unsigned i)
+{
+    if (target_long_bits() == 64) {
+        return p->s64[i];
+    }
+    return p->s32[i];
+}
+
+static inline void target_long_array_set(target_long_array_t *p, unsigned i,
+                                         int64_t v)
+{
+    if (target_long_bits() == 64) {
+        p->s64[i] = v;
+    } else {
+        p->s32[i] = (int32_t)v;
+    }
+}
+
+/* Packed element i; stride is target_long_bits()/8. */
+static inline void *target_ulong_array_elem(const target_ulong_array_t *p,
+                                            unsigned i)
+{
+    if (target_long_bits() == 64) {
+        return (void *)&p->u64[i];
+    }
+    return (void *)&p->u32[i];
+}
 
 #endif /* _TARGET_LONG_H_ */
